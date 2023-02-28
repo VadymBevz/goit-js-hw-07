b15bbbd801bef862d62060d376f2df1e19f10ad1
VadymBevz <vbevzv@gmail.com>
2023-02-28 12:38:08 +0200

    hometask

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryCardsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+    </div>
+    `;
+    }).join('');
+};
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup); 
+const gallery = document.querySelector('.gallery');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
+gallery.addEventListener('click', (event) => {
+  event.preventDefault();
+});
 console.log(galleryItems);
