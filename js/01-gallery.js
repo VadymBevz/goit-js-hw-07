dd3740ad5b291894b211d80b0c4207da193ba9e5
VadymBevz <vbevzv@gmail.com>
2023-02-28 12:24:17 +0200

    photos

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryCardsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+    </div>
+    `;
+    }).join('');
+};
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    
+    const imageUrl = event.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+      <img src="${imageUrl}" width="800" height="600">
+    `);
+    
+    instance.show();
+  }
+}
 console.log(galleryItems);
